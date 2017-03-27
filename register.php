7c16a3f0c1015692408b9d8b7525fe075b1ea665
Skalpu <skalarek66@gmail.com>
2017-03-27 20:31:15 +0200

    Fixed registration validation, resolution scaling, char.creat. heading

---
+++ register.php
@@ -0,0 +1,131 @@
+<?php
+
+    if($_POST)
+    {
+        require_once("config.php");
+		$error_msg = "";
+		$username = $_POST['username'];
+        $password = $_POST['password'];
+        $confirm = $_POST['confirm'];
+
+        $conn = connectDB();
+                
+        $eUsername = $conn->real_escape_string($username);                    
+        $result = $conn->query("SELECT 1 FROM users WHERE UPPER(username) = UPPER('$eUsername')");       
+        $count = $result->num_rows;                                                                              
+
+        if ($count >= 1)
+        {
+			$error_msg = "Taki użytkownik już istnieje.";
+		}
+        else 
+        {
+            $eUsername = $conn->real_escape_string($username);
+            $ehPassword = $conn->real_escape_string(md5($password));
+            $conn->query("INSERT INTO users(username,password) VALUES ('$eUsername','$ehPassword')");
+			
+			header('Location:create_character.php');
+            exit();
+        }
+            
+        $conn->close();  
+    }
+    
+?>
+
+
+
+
+<HTML>
+<Head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="loginregister.css">
+    <Title>SkalpoGra</Title>
+    
+</Head>
+    
+<Body>
+
+    <div id="divWizard"><img id="wizardFoto"></div>
+    <div id="divMonster"><img id="monsterFoto"></div>
+    <div id="divMainOkno">
+		<div id="usernameLabel" class='centerLabel'>Użytkownik:</div>
+		<div id="passwordLabel" class='centerLabel'>Hasło:</div>
+		<div id="confirmLabel" class='centerLabel'>Potwierdź hasło:</div>
+		<div id="errorLabelRegister" class='centerLabel'>		<?php	if(isset($error_msg) && $error_msg != "") { echo $error_msg; }	?>		</div>
+		
+			<Form name="rejestracja" onsubmit="return validateForm();" method="post" action="register.php">
+				<input type='text' class='centerForm' name='username' id='usernameForm'>
+				<input type='password' class='centerForm' name='password' id='passwordForm'>
+				<input type='password' class='centerForm' name='confirm' id='confirmForm'>
+				<input type='submit' class='centerForm' value='Rejestracja' id='submitFormRegister'>
+			</Form>
+        
+    </div>
+    
+    
+    <div id="divLogo"><a href="index.php"><img id="logoFoto"></a></div>
+    <div id="divRejestracja"><a href="register.php"><img id="rejestracjaFoto"></a></div>
+    <div id="divLogowanie"><a href="login.php"><img id="logowanieFoto"></a></div>
+    
+</Body>
+</HTML>
+    
+    
+            
+    
+	
+<script>
+    
+    document.addEventListener('DOMContentLoaded',function()
+    {
+        document.getElementById('usernameForm').focus();
+    });
+        
+        
+    //CLIENT SIDE VALIDATION
+    function validateForm()
+    {
+		var error_msg = "";
+		
+		
+        if (document.forms["rejestracja"]["username"].value === "")
+        {
+			error_msg = "Nazwa użytkownika nie może być pusta!";
+        }
+        else if (document.forms["rejestracja"]["password"].value === "")
+        {
+			error_msg = "Hasło nie może być puste!";
+        }
+		else if (document.forms["rejestracja"]["confirm"].value === "")
+		{
+			error_msg = "Hasło nie może być puste!";
+		}
+		else if (document.forms["rejestracja"]["password"].value !== document.forms["rejestracja"]["confirm"].value)
+        {
+			error_msg = "Hasła nie są jednakowe.";
+        }
+        else if (document.forms["rejestracja"]["password"].value.length < 7)
+        {
+			error_msg = "Hasło musi być dłuższe niż 6 znaków.";    
+        }
+		
+		
+		if(error_msg == "")
+		{
+			alert("true");
+			return true;
+		}
+		else
+		{
+			alert("false");
+			document.getElementById("errorLabelRegister").innerHTML = error_msg;
+			return false;
+		}
+    }
+</script>
+    
+    
+    
