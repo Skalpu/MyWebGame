15c22cd1610811f774e686f512fbe7dda5a509db
Unknown <skalarek66@gmail.com>
2017-04-26 18:26:49 +0200

    Major changes in spells and in their .css

--- spells.php
+++ spells.php
@@ -2,7 +2,7 @@
 
 	class Spell
 	{
-		public $name;
+		public $id;
 		public $level;
 		public $type; //preparation or combat
 		public $subtype; //buff / debuff / damage
@@ -13,9 +13,11 @@
 		public $maxEffect;
 		public $manaCost;
 		public $photo;
+		public $flavor;
+		public $description;
 		
 		//Constructor
-		function __construct($name, $level, $type, $subtype, $statAffected, $aoe, $element, $minEffect, $maxEffect, $manaCost)
+		function __construct($name, $level, $type, $subtype, $statAffected, $aoe, $element, $minEffect, $maxEffect, $manaCost, $flavor, $description)
 		{
 			$this->name = $name;
 			$this->level = $level;
@@ -27,18 +29,20 @@
 			$this->minEffect = $minEffect;
 			$this->maxEffect = $maxEffect;
 			$this->manaCost = $manaCost;
-			$this->photo = "/gfx/spells/$name.jpg";
+			$this->flavor = $flavor;
+			$this->description = $description;
 		}
 	}
 	
 	$preparationSpells = [
-		0 => new Spell("Kamienna skóra", 1, "preparation", "buff", "armor", 1, "earth", 3, 5, 10),
-		1 => new Spell("Odbicie magii", 1, "preparation", "buff", "magicdefense", 1, "air", 3, 5, 10),
-		2 => new Spell("Tarcza płomieni", 1, "preparation", "buff", "reflect", 1, "fire", 2, 3, 15),
-		3 => new Spell("Śliskoskórność", 1, "preparation", "buff", "dodge", 1, "water", 3, 5, 15),
+		0 => new Spell("Kamienna skóra", 1, "preparation", "buff", "armor", 1, "earth", 3, 5, 10, "Czerwony jak cegła...<br>I tak twardy też!", "Twoja skóra pokrywa się skamieliną. Zyskujesz zwiększony pancerz."),
+		1 => new Spell("Magiczna osłona", 1, "preparation", "buff", "magicdefense", 1, "air", 3, 5, 10, "", ""),
+		2 => new Spell("Tarcza płomieni", 1, "preparation", "buff", "reflect", 1, "fire", 2, 3, 15, "", ""),
+		3 => new Spell("Śliskoskórność", 1, "preparation", "buff", "dodge", 1, "water", 3, 5, 15, "", ""),
 	];
 	$combatSpells = [
-		0 => new Spell("Magiczny płomień", 1, "combat", "damage", "hp", 1, "fire", 5, 6, 10),
+		0 => new Spell("Magiczny płomień", 1, "combat", "damage", "hp", 1, "fire", 5, 6, 10, "", ""),
+		1 => new Spell("Rzut głazem", 1, "combat", "damage", "hp", 1, "earth", 4, 8, 10, "", ""),
 	];
 
 ?>
--- update_magic.php
+++ update_magic.php
@@ -4,20 +4,40 @@
 	require_once('spells.php');
 	login_check();
 	
-	drawMagicDropdowns($_SESSION['player']);
-	drawMagicDisplay();
+	drawDropdowns($_SESSION['player']);
+	drawSpellDisplay();
 	
-	function drawMagicDropdowns(Player $player)
+	function drawDropdowns(Player $player)
 	{
 		$preparationAmount = calculatePreparationAmount();
 		$combatAmount = calculateCombatAmount();
 		
+		drawPreparationDropdowns($preparationAmount, $player);
+		drawCombatDropdowns($combatAmount, $player);
+	}
+	
+	function calculatePreparationAmount()
+	{
+		//TODO wpływ rasy i klasy na ilość
+		$amount = 5;
+		return $amount;
+	}
+	
+	function calculateCombatAmount()
+	{
+		//TODO wpływ rasy i klasy na ilość
+		$amount = 5;
+		return $amount;
+	}
+	
+	function drawPreparationDropdowns($amount, Player $player)
+	{
 		//Drawing main container
 		echo "<div id='divPreparation'>";
 		//Drawing title
-		echo "<div id='preparationTitle' class='noselect'>MAGIA PRZYGOTOWAWCZA</div>";
+		echo "<div id='preparationTitle' class='asText centerText whiteText blackShadow noselect'>MAGIA PRZYGOTOWAWCZA</div>";
 		//Drawing dropdowns
-		for($i = 0; $i < $preparationAmount; $i++)
+		for($i = 0; $i < $amount; $i++)
 		{
 			//Preparing button text
 			if($player->preparationMagic[$i] != ""){
@@ -29,62 +49,114 @@
 			
 			//Drawing dropdown
 			echo "<div class='dropdown'>";
-			
 				//Drawing dropdown button
-				echo "<div class='dropdownButton orange arrow noselect'>$buttonText</div>";
+				echo "<div class='dropdownButton asText bold orange arrow noselect'>$buttonText</div>";
 				unset($buttonText);
-				
-					//Drawing dropdown content
-					echo "<div id='preparationContent$i' class='dropdownContent'>";					
-					//Drawing spells
-					foreach ($GLOBALS['preparationSpells'] as $key => $spell)
+				//Drawing dropdown content
+				echo "<div id='preparationContent$i' class='scrollable dropdownContent asText centerText'>";					
+				//Drawing spells
+				foreach ($GLOBALS['preparationSpells'] as $key => $spell)
+				{
+					//Checking if player has access to the spell
+					if($player->village['magetower'] >= $spell->level)
 					{
-						//Checking if player has access to the spell
-						if($player->village['magetower'] >= $spell->level)
-						{
-							//Checking if this isn't the last dropdown
-							if($key == (count($GLOBALS['preparationSpells']) - 1)){
-								$class = "dropdownLast";
-							}
-							else{
-								$class = "";
-							}
-							//Checking the element of the spell
-							$element = $spell->element;
-							
-							echo "<div class='dropdownOption $class whiteGradient arrow noselect'><div class='icon $element'></div><div class='spellName'>" . $spell->name . "</div></div>";
-							unset($class);
-							unset($element);
-						}
+						//Checking the element of the spell
+						$element = $spell->element;
+						
+						//Drawing dropdown option
+						echo "<div class='dropdownOption whiteGradientHover arrow noselect'>";
+							echo "<div class='icon $element'></div>";
+							echo "<div class='spellName'>" . $spell->name . "</div>";
+							echo "<div class='spellFlavor' style='display:none'>" .$spell->flavor. "</div>";
+							echo "<div class='spellDescription' style='display:none'>" .$spell->description. "</div>";
+						echo "</div>";
+						unset($element);
 					}
-					//End of dropdown content
-					echo "</div>";
+				}
+				//End of dropdown content
+				echo "</div>";
 			//End of dropdown
 			echo "</div>";
 		}
-		
 		//End of main container
 		echo "</div>";
 	}
 	
-	function drawMagicDisplay()
+	function drawCombatDropdowns($amount, Player $player)
 	{
-		
+		//Drawing main container
+		echo "<div id='divCombat'>";
+		//Drawing title
+		echo "<div id='combatTitle' class='asText centerText whiteText blackShadow noselect'>MAGIA BITEWNA</div>";
+		//Drawing dropdowns
+		for($i = 0; $i < $amount; $i++)
+		{
+			//Preparing button text
+			if($player->combatMagic[$i] != ""){
+				$buttonText = "#" . ($i+1) . $player->combatMagic[$i]->name;
+			}
+			else{
+				$buttonText = "Czar bitewny #" . ($i+1);
+			}
+			
+			//Drawing dropdown
+			echo "<div class='dropdown'>";
+				//Drawing dropdown button
+				echo "<div class='dropdownButton asText bold orange arrow noselect'>$buttonText</div>";
+				unset($buttonText);
+				//Drawing dropdown content
+				echo "<div id='combatContent$i' class='scrollable dropdownContent asText centerText'>";
+				//Drawing spells
+				foreach ($GLOBALS['combatSpells'] as $key => $spell)
+				{
+					//Checking if player has access to the spell
+					if($player->village['magetower'] >= $spell->level)
+					{
+						//Checking the element of the spell
+						$element = $spell->element;
+							
+						//Drawing dropdown option
+						echo "<div class='dropdownOption whiteGradientHover arrow noselect'>";
+							echo "<div class='icon $element'></div>";
+							echo "<div class='spellName'>" . $spell->name . "</div>";
+							echo "<div class='spellFlavor' style='display:none'>" .$spell->flavor. "</div>";
+							echo "<div class='spellDescription' style='display:none'>" .$spell->description. "</div>";
+						echo "</div>";
+						unset($element);
+					}
+				}
+				//End of dropdown content
+				echo "</div>";
+			//End of dropdown
+			echo "</div>";
+		}
+		//End of main container
+		echo "</div>";
 	}
 	
-	function calculatePreparationAmount()
+	function drawSpellDisplay()
 	{
-		//TODO wpływ rasy i klasy na ilość
-		$amount = 5; //$_SESSION['player']->village['magetower'];
-		return $amount;
+		//Main container
+		echo "<div id='divSpell'>";
+			//Spell title
+			echo "<div id='spellTitle' class='bold fullWidth whiteGradient whiteShadow paddingVertical blackBorder centerText borderBox'>Kamienna skóra</div>";
+			//Spell photo container
+			echo "<div id='spellPhoto' class='fullWidth blackBorder borderBox'></div>";
+			//Spell footer
+			echo "<div id='spellFooter' class='fullWidth blackBorder whiteGradient paddingFull borderBox scrollable'>";
+				//Spell flavor
+				echo "<div id='spellFlavor' class='italic centerText'>Czerwony jak cegła...<br>I tak twardy też!</div>";
+				//Divider
+				echo "<div class='divider'></div>";
+				//Spell description
+				echo "<div id='spellDescription' class='centerText'>Twoja skóra pokrywa się skamieliną. Zyskujesz zwiększony pancerz.</div>";
+			//End of spell footer
+			echo "</div>";
+		//End of main container
+		echo "</div>";
 	}
 	
-	function calculateCombatAmount()
-	{
-		//TODO wpływ rasy i klasy na ilość
-		$amount = 5; //$_SESSION['player']->village['magetower'];
-		return $amount;
-	}
+	
 
 ?>
 
@@ -92,6 +164,8 @@
 
 	$("#divPlayerBars").load('update_player_bars.php');
 	initializeDropdowns();
+	initializeHover();
+
 	
 	function initializeDropdowns()
 	{
@@ -99,18 +173,17 @@
 		$(".dropdownButton").click(function(e){
 			//Checking if dropdown is currently shown or hidden
 			var current = $(this).parent().find(".dropdownContent").css("display");
-		
+
 			//Show or hide accordingly
 			if(current == "none"){
-				var next = "block";
+				var next = "inline-block";
 				$(".dropdownContent").css("display", "none");
 			}
 			else{
 				var next = "none";
 			}
-			
+
 			$(this).parent().find(".dropdownContent").css("display", next);
-		
 			e.stopPropagation();
 		});
 
@@ -125,5 +198,26 @@
 			$(".dropdownContent").hide();
 		});
 	}
+	
+	function initializeHover()
+	{
+		$(".dropdownOption").hover(
+			function(){
+				//Getting required variables
+				var type = $(this).parent().attr('id');
+				var spellName = $(this).find('.spellName').html();
+				var spellFlavor = $(this).find('.spellFlavor').html();
+				var spellDescription = $(this).find('.spellDescription').html();
+				var spellImage = "url('gfx/spells/" + spellName + ".jpg')";
+				
+				//Setting
+				$("#spellTitle").html(spellName);
+				$("#spellFlavor").html(spellFlavor);
+				$("#spellDescription").html(spellDescription);
+				$("#spellPhoto").css("background-image", spellImage);
+			}
+		);
+	}
+	
 
 </script>
